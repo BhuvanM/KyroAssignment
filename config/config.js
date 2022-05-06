d93d8bc6e7749222c37ba51db286f3cdb95ab093
BhuvaneshwarM <bhuvaneshwar.m@gmail.com>
2022-05-06 23:19:46 +0530

    psk removal

--- config/config.js
+++ config/config.js
@@ -1,7 +1,6 @@
 const Sequelize = require("sequelize");
 let path = require('path');
 const createTable = require(path.resolve('./models'))
-let psk = '161552b9a6a2cab4ad17d2a8b5399cb6df8201435d9b7ba8b3a7304929d8e92f';
 
 console.log("DB CREDS### ",process.env.DATABASE_URL);
 const sequelize = new Sequelize(process.env.DATABASE_URL,{
